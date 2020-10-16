338ee4605b5fe69eb16c5d946670c3113cea45be
linncode <193265261@qq.com>
2020-10-16 11:00:31 +0800

    更新404界面

---
+++ src/components/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="notFound">
+        404
+    </div>
+</template>
+
+<style lang="less" scoped>
+#notFound{
+    display: grid;
+    place-content: center center;
+    height: 100vh;
+    background-color:rgba(255,255,255,.5);
+    font-size: 100px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,22 @@
-const Nav = () => import(/* webpackChunkName: "nav_link" */ '../components/Nav.vue')
-const Link = () => import(/* webpackChunkName: "nav_link" */ '../components/Link.vue')
+const Nav = () => import(/* webpackChunkName: "nav_link_notfound" */ '../components/Nav.vue')
+const Link = () => import(/* webpackChunkName: "nav_link_notfound" */ '../components/Link.vue')
+const NotFound = () => import(/* webpackChunkName: "nav_link_notfound" */'../components/NotFound.vue')
 
 Vue.use(VueRouter)
 
 const routes = [
-  {path: '/',redirect: '/link'},
-  {path: '/nav',component:Nav,children:[
-    {path:'/link',component:Link}
-  ]}
+  { path: '*', component: NotFound },
+  { path: '/',redirect: '/link'},
+  { path: '/nav',
+    component:Nav,
+    children:[
+      { path:'/link',component:Link}
+    ]
+  }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
